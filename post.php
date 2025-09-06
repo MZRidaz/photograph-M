4e6ba5568ea3a61734aa7ae75dab882603c8add7
MZRidaz <chenyin653@gmail.com>
2025-09-06 08:14:15 +0800

    Update post.php

--- post.php
+++ post.php
@@ -11,90 +11,14 @@
     ?>
 </div>
 
-<div class="post-info">
-    <div class="post-info-box"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span><span class="post-info-title anti-select">标题：</span><span class="post-info-text"><?php echo $this->title ?></span></div>
-    <div class="post-info-box"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span><span class="post-info-title anti-select">分类：</span><span class="post-info-text"><?php $this->category(','); ?></span></div>
-<!-- 模特主页链接显示 -->
-<div class="post-info-box">
-    <span class="glyphicon glyphicon-link" aria-hidden="true"></span>
-    <span class="post-info-title anti-select"><strong>模特主页：</strong></span>
-    <span class="post-info-text">
-        <?php
-        // 检查是否有任何社交链接
-        $has_social = false;
-        
-        // 判断推特主页是否填写
-        if ($this->fields->src_twitter && $this->fields->src_twitter != ''):
-            $has_social = true; ?>
-            <a href="<?php echo $this->fields->src_twitter; ?>" target="_blank">推特</a>
-        <?php endif; ?>
-        
-        <!-- 判断 Instagram 主页是否填写 -->
-        <?php if ($this->fields->src_instagram && $this->fields->src_instagram != ''):
-            $has_social = true; ?>
-            <a href="<?php echo $this->fields->src_instagram; ?>" target="_blank">Instagram</a>
-        <?php endif; ?>
-        
-        <!-- 判断抖音主页是否填写 -->
-        <?php if ($this->fields->src_douyin && $this->fields->src_douyin != ''):
-            $has_social = true; ?>
-            <a href="<?php echo $this->fields->src_douyin; ?>" target="_blank">抖音</a>
-        <?php endif; ?>
-        
-        <!-- 判断微博主页是否填写 -->
-        <?php if ($this->fields->src_weibo && $this->fields->src_weibo != ''):
-            $has_social = true; ?>
-            <a href="<?php echo $this->fields->src_weibo; ?>" target="_blank">微博</a>
-        <?php endif; ?>
-        
-        <!-- 判断 其他主页 是否填写 -->
-        <?php if ($this->fields->src_other && $this->fields->src_other != ''):
-            $has_social = true; ?>
-            <a href="<?php echo $this->fields->src_other; ?>" target="_blank">其他</a>
-        <?php endif; ?>
-        
-        <!-- 如果没有任何社交链接，显示"暂无" -->
-        <?php if (!$has_social): ?>
-            暂无
-        <?php endif; ?>
-    </span>
-</div>
+	<div class="post-info">
+		<div class="post-info-box"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span><span class="post-info-title anti-select">标题：</span><span class="post-info-text"><?php echo $this->title ?></span></div>
+		<div class="post-info-box"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span><span class="post-info-title anti-select">拍摄/来源：</span><span class="post-info-text"><?php echo $this->fields->photog != "" ? ($this->fields->srcurl != "" ? '<a href="'.$this->fields->srcurl.'" target="_blank">'.$this->fields->photog.'</a>' : $this->fields->photog) : '未填写' ?></span></div>
+		<div class="post-info-box"><span class="glyphicon glyphicon-user" aria-hidden="true"></span><span class="post-info-title anti-select">出镜：</span><span class="post-info-text"><?php echo $this->fields->appear != "" ? $this->fields->appear : '未知' ?></span></div>
+		<div class="post-info-box"><span class="glyphicon glyphicon-adjust" aria-hidden="true"></span><span class="post-info-title anti-select">处理软件：</span><span class="post-info-text"><?php echo afterSoftware()[$this->fields->software] ?></span></div>
+		<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span><span class="post-info-title anti-select">描述：</span><span class="post-info-text"><?php echo $this->fields->description != "" ? $this->fields->description : ($this->options->picdesc ? $this->options->picdesc : '未填写') ?></span>
+	</div>
 
-<!-- 下载地址字段显示 -->
-<div class="post-info-box">
-    <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span>
-    <span class="post-info-title anti-select"><strong>下载地址：</strong></span>
-    <span class="post-info-text">
-        <?php
-        // 检查是否有任何下载链接
-        $has_download = false;
-        
-        // 检查 TeraBox 地址是否填写
-        if ($this->fields->terabox_url && $this->fields->terabox_url != ''):
-            $has_download = true; ?>
-            <a href="<?php echo $this->fields->terabox_url; ?>" target="_blank">TeraBox</a>
-        <?php endif; ?>
-
-        <!-- 检查 夸克 地址是否填写 -->
-        <?php if ($this->fields->quark_url && $this->fields->quark_url != ''):
-            $has_download = true; ?>
-            <a href="<?php echo $this->fields->quark_url; ?>" target="_blank">夸克</a>
-        <?php endif; ?>
-
-        <!-- 检查 百度网盘 地址是否填写 -->
-        <?php if ($this->fields->baidu_url && $this->fields->baidu_url != ''):
-            $has_download = true; ?>
-            <a href="<?php echo $this->fields->baidu_url; ?>" target="_blank">百度网盘</a>
-        <?php endif; ?>
-        
-        <!-- 如果没有任何下载链接，显示"暂无" -->
-        <?php if (!$has_download): ?>
-            暂无
-        <?php endif; ?>
-    </span>
-</div>
-
-</div>
 
 <?php if ($this->is('post')): ?>
     <div class="related-posts">
